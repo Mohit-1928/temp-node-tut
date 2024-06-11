45895f5e8e61140bf4263eb3d6f399a813d9264b
Mohit kumar sharma <kumarsharmamohit67@gmail.com>
2024-06-11 10:42:06 +0530

    First commit

---
+++ 2-Globals.js
@@ -0,0 +1,5 @@
+console.log(__dirname);
+
+setInterval(() => {
+    console.log("Hello World")
+}, 1000)
---
+++ 3-Modules.js
@@ -0,0 +1,8 @@
+// Modules
+const names = require('./4-name')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-Mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
